106fe91f91a5633bdbd8d1b998ce1b79282945a3
loristrr <loristerry63430@gmail.com>
2023-04-04 09:18:07 +0200

    slider

--- src/app/components/identification/slider/slider.component.ts
+++ src/app/components/identification/slider/slider.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
-
+  imageObject = [{
+    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
+},{
+    image: 'https://www.comment-apprendre-la-photo.fr/wp-content/uploads/2020/09/Comment-apprendre-la-photo-bonnes-photos-d-oiseaux-6-pexels-pixabay-416179.jpg',
+    thumbImage: 'https://www.comment-apprendre-la-photo.fr/wp-content/uploads/2020/09/Comment-apprendre-la-photo-bonnes-photos-d-oiseaux-6-pexels-pixabay-416179.jpg',
+}];
 }
+
