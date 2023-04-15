8610e6f7049bb06a3ef3bd709f8b42148477d0b9
Sinsay63 <yanis.houdier@gmail.com>
2023-04-15 17:10:58 +0200

    ajout link api + exemple

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User, UserService, Users } from 'app/api';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-explore',
   templateUrl: './explore.component.html',
   styleUrls: ['./explore.component.css']
 })
-export class ExploreComponent {
+export class ExploreComponent implements OnInit{
 
+  //attributs
+  users ?= new Array<User>();
+
+
+  constructor(private userService: UserService) {
+   }
+
+
+  ngOnInit(): void {
+      
+    this.userService.getUsers().subscribe(data => {
+      console.log(data);
+      this.users = data.users;
+    }, (error => {
+      console.log(error);
+    }));
+  }
+
+  // async truc() {
+  //   this.serService.getUsers().subscribe(data => {
+  //     console.log(data);
+  //   }, (error => {
+  //     console.log(error);
+  //   }));
+
+  //  await firstValueFrom(this.serService.getUsers()).then(data => {
+  //     this.users = data;
+  //   }, (error => {
+  //     console.log(error);
+  //   }));
+  // }
 }
