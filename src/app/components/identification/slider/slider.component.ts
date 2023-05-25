5509afd3d924936542a8373f7224937180731dd9
loristrr <loristerry63430@gmail.com>
2023-05-25 19:19:09 +0200

    nouveau login et register

--- src/app/components/identification/slider/slider.component.ts
+++ src/app/components/identification/slider/slider.component.ts
@@ -1,17 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, UserRegister } from 'app/api';
+import { GlobalDataService } from 'app/global.data.service';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: ['./slider.component.scss']
 })
 export class SliderComponent {
-  imageObject = [{
-    image: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-    thumbImage: 'https://sanjayv.github.io/ng-image-slider/contents/assets/img/slider/5.jpg',
-},{
-    image: 'https://www.comment-apprendre-la-photo.fr/wp-content/uploads/2020/09/Comment-apprendre-la-photo-bonnes-photos-d-oiseaux-6-pexels-pixabay-416179.jpg',
-    thumbImage: 'https://www.comment-apprendre-la-photo.fr/wp-content/uploads/2020/09/Comment-apprendre-la-photo-bonnes-photos-d-oiseaux-6-pexels-pixabay-416179.jpg',
-}];
-}
+  @HostBinding('class') className = 'sliderComp';
 
+  username: string = '';
+  lastname: string = '';
+  firstname: string = '';
+  email: string = '';
+  password: string = '';
+  birthday: string= '';
+
+  constructor(private globalDataService : GlobalDataService, private authService: AuthService, private router: Router) {
+  }
+
+  async register(){
+    try{
+      var user = new UserRegister()
+      if(this.username != '' && this.email != '' && this.birthday != '' && this.password != '' ){
+
+        user.username = this.username
+        user.firstname = this.firstname
+        user.lastname = this.lastname
+        user.email = this.email
+        user.birthday = this.birthday
+        user.password = this.password
+
+        const data = await this.authService.registerUser(user).toPromise();
+
+        if(data?.token != null){
+          sessionStorage.setItem('token', data?.token);
+        }
+        this.router.navigateByUrl('/', {skipLocationChange: false}).then(() => {
+          this.router.navigate(['home']);
+        });
+      } 
+    }catch (error) {
+      console.log("erreur lors de l'inscription")
+    }
+  }
+}
